8819fbbf1b3609581b66ddde4dd87097a44cb354
Nacho <ignaciofiliba@gmail.com>
2026-01-10 00:21:06 -0300

    Agregada autenticación JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StockModule } from './stock/stock.module';
 import { Producto } from './stock/producto.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { Producto } from './stock/producto.entity';
       synchronize: true,
     }),
     StockModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
